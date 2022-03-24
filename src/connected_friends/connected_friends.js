91a3092e562c4bd59a675b151efa4008c4ffe140
johmert <johmert@users.noreply.github.com>
2022-03-24 03:55:36 -0700

    parentheses completed

--- src/connected_friends/connected_friends.js
+++ src/connected_friends/connected_friends.js
@@ -1,5 +1,7 @@
 const Queue = require("../lib/queue");
 
-const connected = (G, s, r) => {};
+const connected = (G, s, r) => {
+    
+};
 
 module.exports = connected;
--- src/parentheses/parentheses.js
+++ src/parentheses/parentheses.js
@@ -1,5 +1,24 @@
 const Stack = require("../lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+    const stack = new Stack();
+    for(let i=0, length = expression.length; i<length; i++){
+        if(expression[i] === '('){
+            stack.push(expression[i]);
+        }
+        if(expression[i] === ')'){
+            if(stack.top){
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    if(!stack.top){
+        return true
+    } else {
+        return false;
+    }
+};
 
 module.exports = match;
