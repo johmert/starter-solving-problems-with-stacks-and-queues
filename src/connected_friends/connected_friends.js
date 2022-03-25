ad4645c22727729f2b288e5dc75c0d3c158e68d8
johmert <johmert@users.noreply.github.com>
2022-03-24 23:34:54 -0700

    connected friends completed

--- src/connected_friends/connected_friends.js
+++ src/connected_friends/connected_friends.js
@@ -1,7 +1,26 @@
 const Queue = require("../lib/queue");
 
-const connected = (G, s, r) => {
-    
+const connected = (graph, startUser, endUser) => {
+    if(Object.keys(graph).length === 0) return false;
+    if(endUser === startUser) return true;
+
+    const enqueued = [startUser];
+    const discovered = new Queue();
+
+    discovered.enqueue(startUser);
+
+    let result = false;
+    while(discovered.first){
+        let user = discovered.dequeue();
+        graph[user].forEach(followedUser => {
+            if(followedUser === endUser) result = true;
+            if(!enqueued.includes(followedUser)) {
+                enqueued.push(followedUser);
+                discovered.enqueue(followedUser)
+            }
+        })
+    }
+    return result;
 };
 
 module.exports = connected;
